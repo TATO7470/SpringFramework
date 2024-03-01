c5e2fc40b4b853713632c462e5d96b32364f1ac2
tato7470 <tato74701337@gmail.com>
2024-03-01 12:43:02 +0300

    NO HANDLER

--- src/main/java/com/example/UserException/UserController.java
+++ src/main/java/com/example/UserException/UserController.java
@@ -2,6 +2,9 @@ package com.example.UserException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
 
 @RestController
 public class UserController {
@@ -9,20 +12,16 @@ public class UserController {
     public ResponseEntity<?> getUser(){
         return ResponseEntity.ok(new User("tato","tato7470"));
     }
-//    @PostMapping("/postUser")
-//    public ResponseEntity<?> postUser(@RequestBody User user){
-//        return ResponseEntity.ok(user);
-//    }
-
     @PostMapping("/postUser")
-    public ResponseEntity<?> postUser(@RequestBody User user) {
-        if (user.getLogin() == null || user.getPassword() == null) {
-            throw new UserException("Логин и пароль не найдены");
+    public ResponseEntity<?> post(@RequestBody Map<String, String> request) {
+        try {
+            if (request.size() > 10 || !request.containsKey("login") || !request.containsKey("password")) {
+                throw new Exception("Логин или пароль не найдены");
+            }
+            User user = new User(request.get("login"), request.get("password"));
+            return ResponseEntity.ok(user);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
-        return ResponseEntity.ok(user);
-    }
-    @ExceptionHandler( UserException.class)
-    public ResponseEntity<?> postException(UserException ue) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ue.getMessage());
     }
 }
--- src/main/java/com/example/UserException/UserException.java
+++
@@ -1,7 +0,0 @@
-package com.example.UserException;
-
-public class UserException extends RuntimeException {
-    public UserException(String message) {
-        super(message);
-    }
-}
